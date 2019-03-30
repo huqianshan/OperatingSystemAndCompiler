dddc34c679571427dc310165fcc13c90b07ee8c5
Ubuntu <huqianshan_uestc@163.com>
2019-03-30 18:32:23 +0800

    lexical

--- Compiler/test.c
+++ Compiler/test.c
@@ -2,12 +2,36 @@
 // what 
 /* abcee */
 
-int main( ){  //d d ddd
+typedef enum {
+    T_Le = 256, T_Ge, T_Eq, T_Ne, T_And, T_Or, T_IntConstant,
+    T_StringConstant, T_Identifier, T_Void, T_Int, T_While,
+    T_If, T_Else, T_Return, T_Break, T_Continue, T_Print,
+    T_ReadInt
+} TokenType;
+
+static void print_token(int token) {
+    static char* token_strs[] = {
+        "T_Le", "T_Ge", "T_Eq", "T_Ne", "T_And", "T_Or", "T_IntConstant",
+        "T_StringConstant", "T_Identifier", "T_Void", "T_Int", "T_While",
+        "T_If", "T_Else", "T_Return", "T_Break", "T_Continue", "T_Print",
+        "T_ReadInt"
+    };
+
+    if (token < 256) {
+        printf("%-20c", token);
+    } else {
+        printf("%-20s", token_strs[token-256]);
+    }
+}
+
+int main(int argc,char **argv ){  //d d ddd
     /* a
     b
     c
     */
-    char n = '/';
-    printf("%d\n",n);
+    char token = "";
+    int n;
+    while (scanf("%d",&n))
+        print_token(n);
     return 0;
 }
