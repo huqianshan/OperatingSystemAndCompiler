cb8fa448d687eaf889fd31b47fc474b36eda8ec2
ubuntu18.04 <1196455147@qq.com>
2020-04-30 20:23:35 +0800

    :white_check_mark: rand test

--- drivers/NVMSIM/test/fio/fio.sh
+++ drivers/NVMSIM/test/fio/fio.sh
@@ -5,12 +5,12 @@
 
 #sudo rm -f /mnt/nvm0/*
 sudo fio -filename=/dev/nvm0 -direct=1 -iodepth $1 -thread \
- -rw=read -ioengine=libaio -bs=$2k -size=$3G -numjobs=$4 \
+ -rw=randread -ioengine=libaio -bs=$2k -size=$3G -numjobs=$4 \
  -runtime=1000 -group_reporting -name=my_nvm_read_test \
- > ./output/nvm/nvm-readSeq-$1-$2k-$3G-test;
+ > ./output/nvmrand/nvm-readRand-$1-$2k-$3G-test;
 
 #sudo rm -f /mnt/nvm0/*;
 sudo fio -filename=/dev/nvm0 -direct=1 -iodepth $1 -thread \
--rw=write -ioengine=libaio -bs=$2k -size=$3G -numjobs=$4 \
+-rw=randwrite -ioengine=libaio -bs=$2k -size=$3G -numjobs=$4 \
 -runtime=1000 -group_reporting -name=my_nvm_write_test \
-> ./output/nvm/nvm-writeSeq-$1-$2k-$3G-test;
+> ./output/nvmrand/nvm-writeRand-$1-$2k-$3G-test;
